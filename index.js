de15ef11af3b72c64d4f2fa5ef245e3d04de3334
vitoriopaulo <vitoriopaulo@yahoo.com>
2020-05-31 21:28:40 -0300

    commit

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
- $(() => {
-  	$('a[href^="#"]').on('click',function (e) {
-  	    e.preventDefault();
+$(() => {
+   $('a[href^="#"]').on('click',function (e) {
+       e.preventDefault();
 
-  	    var target = this.hash;
-  	    var $target = $(target);
+       var target = this.hash;
+       var $target = $(target);
 
-  	    $('html, body').stop().animate({
-  	        'scrollTop': $target.offset().top
-  	    }, 900, 'swing', function () {
-  	        window.location.hash = target
-	    })
-  	})
- })
+       $('html, body').stop().animate({
+           'scrollTop': $target.offset().top
+       }, 900, 'swing', function () {
+           window.location.hash = target
+       })
+   })
+
+   $(window).scroll(function(){
+           if ($(this).scrollTop() > 100) {
+               $('#scroll').fadeIn();
+           } else {
+               $('#scroll').fadeOut();
+           }
+       });
+       $('#scroll').click(function(){
+           $("html, body").animate({ scrollTop: 0 }, 600);
+           return false;
+    });
+})
 
 // Imported from my codepen.io Account //
-
-// $(document).ready(function(){
-// 	$('a[href^="#"]').on('click',function (e) {
-// 	    e.preventDefault();
-//
-// 	    var target = this.hash;
-// 	    var $target = $(target);
-//
-// 	    $('html, body').stop().animate({
-// 	        'scrollTop': $target.offset().top
-// 	    }, 900, 'swing', function () {
-// 	        window.location.hash = target;
-// 	    });
-// 	});
-// });
